b15871d4ea8d8c7a3bfdf136f3abca84817e5d6e
Luca Liebenberg <liebenbergluca@gmail.com>
2022-10-19 21:53:43 +0200

    initial commit

---
+++ LoginScreenUI/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  LoginScreenUI
+//
+//  Created by Luca Liebenberg on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
